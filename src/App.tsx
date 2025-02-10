15601492d771791921af1cc54005cbcc0e671264
Arthur Lima <artglima.dev@gmail.com>
2025-02-10 20:42:27 -0300

    feat: application structure - created Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { Header } from './components/Header'
+import { Input } from './components/Input'
 import './global.css'
 
 export function App() {
   return (
+    <main>
       <Header />
+      <Input />
+    </main>
+      
   )
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Input.module.css'
+
+export function Input() {
+  return (
+    <input
+      className={styles.container}
+      placeholder="Adicione uma nova tarefa"
+    />
+  );
+}
