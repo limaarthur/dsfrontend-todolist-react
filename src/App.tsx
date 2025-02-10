bc97838ffd86413e3c9227e71daa9d52a208593e
Arthur Lima <artglima.dev@gmail.com>
2025-02-10 20:59:12 -0300

    feat: application structure - created Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { PlusCircle } from '@phosphor-icons/react'
+
+import { Button } from './components/Button'
 import { Header } from './components/Header'
 import { Input } from './components/Input'
+
 import './global.css'
 
 export function App() {
   return (
     <main>
       <Header />
-      <Input />
-    </main>
       
+      <section>
+        <div>
+          <Input />
+          <Button>
+            Criar
+            <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+          </Button>
+        </div>
+      </section>
+    </main>
   )
 }
